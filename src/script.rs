10cf53c70cc7a8fea8befee4f5d54181ae248bb2
Yurii Rashkovskii <yrashk@gmail.com>
2017-02-05 01:27:19 +0700

    Problem: script composition is possible only over stack
    However, there will be cases where being able to wrap fragments of code and
    call them from other programs is highly beneficial.
    
    For example, being able to wrap a piece of code into a transaction. Or, even
    simpler, run a loop.
    
    Solution: introduce EVAL word that will take topmost value off the stack
    and evaluate it as a program, using current stack.
    
    Also, add [] syntax to the parser to allow instructions like this:
    
    ```
    0x10 [DUP DUP] EVAL
    ```

--- src/script.rs
+++ src/script.rs
@@ -97,6 +97,13 @@ word!(ROT, (a, b, c  => b, c, a), b"\x83ROT");
 /// `OVER` copies the second topmost item to the top of the stack
 word!(OVER, (a, b => a, b, a), b"\x84OVER");
 
+
+// Control flow
+
+/// `EVAL` takes the topmost item and evaluates it as a PumpkinScript
+/// program on the current stack
+word!(EVAL, b"\x84EVAL");
+
 /// `Instruction` is a type alias for `Vec<u8>` for a single instruction (be it a word or data)
 ///
 /// # Data Representation
@@ -174,6 +181,169 @@ macro_rules! push {
     };
 }
 
+mod binparser {
+    use nom::{IResult, Needed, ErrorKind};
+    use script::Program;
+
+
+    fn to_vec(s: &[u8]) -> Vec<u8> {
+        let mut vec = Vec::new();
+        vec.extend_from_slice(s);
+        vec
+    }
+
+    pub fn word_tag(i: &[u8]) -> IResult<&[u8], u8> {
+        if i.len() < 1 {
+            IResult::Incomplete(Needed::Size(1))
+        } else if i[0] & 128 != 128 {
+            IResult::Error(ErrorKind::Custom(128))
+        } else {
+            IResult::Done(&i[0..], i[0] - 128 + 1)
+        }
+    }
+
+    pub fn micro_length(i: &[u8]) -> IResult<&[u8], usize> {
+        if i.len() < 1 {
+            IResult::Incomplete(Needed::Size(1))
+        } else if i[0] > 120 {
+            IResult::Error(ErrorKind::Custom(120))
+        } else {
+            let size = i[0] as usize;
+            if size > i.len() - 1 {
+                IResult::Incomplete(Needed::Size(1 + size))
+            } else {
+                IResult::Done(&i[0..], size + 1)
+            }
+        }
+    }
+
+    pub fn byte_length(i: &[u8]) -> IResult<&[u8], usize> {
+        if i.len() < 2 {
+            IResult::Incomplete(Needed::Size(2))
+        } else if i[0] != 121 {
+            IResult::Error(ErrorKind::Custom(121))
+        } else {
+            let size = i[1] as usize;
+            if size > i.len() - 2 {
+                IResult::Incomplete(Needed::Size(2 + size))
+            } else {
+                IResult::Done(&i[0..], size + 2)
+            }
+        }
+    }
+
+    pub fn small_length(i: &[u8]) -> IResult<&[u8], usize> {
+        if i.len() < 3 {
+            IResult::Incomplete(Needed::Size(3))
+        } else if i[0] != 122 {
+            IResult::Error(ErrorKind::Custom(122))
+        } else {
+            let size = (i[1] as usize) << 8 | i[2] as usize;
+            if size > i.len() - 3 {
+                IResult::Incomplete(Needed::Size(3 + size))
+            } else {
+                IResult::Done(&i[0..], size + 3)
+            }
+        }
+    }
+
+    pub fn big_length(i: &[u8]) -> IResult<&[u8], usize> {
+        if i.len() < 5 {
+            IResult::Incomplete(Needed::Size(5))
+        } else if i[0] != 123 {
+            IResult::Error(ErrorKind::Custom(123))
+        } else {
+            let size = (i[1] as usize) << 24 | (i[2] as usize) << 16 | (i[3] as usize) << 8 |
+                       (i[4] as usize);
+            if size > i.len() - 5 {
+                IResult::Incomplete(Needed::Size(5 + size))
+            } else {
+                IResult::Done(&i[0..], size + 5)
+            }
+        }
+    }
+    named!(data<Vec<u8>>, do_parse!(
+                                  data: length_bytes!(alt!(micro_length |
+                                                           byte_length  |
+                                                           small_length |
+                                                           big_length))  >>
+                                        (to_vec(data))));
+
+    named!(word<Vec<u8>>, do_parse!(
+                                 word: length_bytes!(word_tag)      >>
+                                       (to_vec(word))));
+
+    named!(split_code<Vec<Vec<u8>>>, many0!(alt!(word | data)));
+
+    /// Parse single Vec<u8> into separate instructions (a program)
+    pub fn parse(code: Vec<u8>) -> Program {
+        let (_, x) = split_code(code.as_slice()).unwrap();
+        x
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use script::parse as parse_text;
+        use script::binparser::parse;
+
+        #[test]
+        fn test() {
+            let v = parse_text("0x10 DUP");
+            let mut vec = Vec::new();
+            for mut item in v {
+                vec.append(&mut item);
+            }
+            assert_eq!(parse(vec), parse_text("0x10 DUP"));
+        }
+
+        #[test]
+        fn test_byte() {
+            let mut byte_sized_sequence: String = "0x".to_owned();
+            for _ in 1..200 {
+                byte_sized_sequence.push_str("AA");
+            }
+            let v = parse_text(byte_sized_sequence.as_ref());
+            let mut vec = Vec::new();
+            for mut item in v {
+                vec.append(&mut item);
+            }
+            assert_eq!(parse(vec), parse_text(byte_sized_sequence.as_ref()));
+        }
+
+        #[test]
+        fn test_small() {
+            let mut byte_sized_sequence: String = "0x".to_owned();
+            for _ in 1..300 {
+                byte_sized_sequence.push_str("AA");
+            }
+            let v = parse_text(byte_sized_sequence.as_ref());
+            let mut vec = Vec::new();
+            for mut item in v {
+                vec.append(&mut item);
+            }
+            assert_eq!(parse(vec), parse_text(byte_sized_sequence.as_ref()));
+        }
+
+        #[test]
+        fn test_big() {
+            let mut byte_sized_sequence: String = "0x".to_owned();
+            for _ in 1..70000 {
+                byte_sized_sequence.push_str("AA");
+            }
+            let v = parse_text(byte_sized_sequence.as_ref());
+            let mut vec = Vec::new();
+            for mut item in v {
+                vec.append(&mut item);
+            }
+            assert_eq!(parse(vec), parse_text(byte_sized_sequence.as_ref()));
+        }
+
+    }
+
+}
+
+pub use self::binparser::parse as parse_bin;
+
 fn run(tuple: (Stack, Program)) -> BoxFuture<(Stack, Program), Error> {
     let (mut stack, mut program) = tuple;
     if program.len() > 0 {
@@ -222,6 +392,10 @@ fn run(tuple: (Stack, Program)) -> BoxFuture<(Stack, Program), Error> {
                 stack.push(b);
                 stack.push(a);
                 stack.push(c);
+            }
+            &[ref body..] if body == EVAL => {
+                let code = pop_or_fail!(stack, program);
+                return execute(stack, parse_bin(code)).and_then(|(s, _)| run((s, program))).boxed();
             }
             &[sz @ 129u8...255u8, ref body..] if body.len() == (sz ^ 128u8) as usize => {
                 let mut vec = Vec::new();
@@ -394,6 +568,20 @@ mod tests {
         Some(Error::EmptyStack(_))));
     }
 
+    #[test]
+    fn eval() {
+        let (mut stack, _) =
+            execute(Vec::new(), parse("[0x01 DUP [DUP] EVAL] EVAL DROP")).wait().unwrap();
+        assert_eq!(stack.pop().unwrap(), vec![1]);
+        assert_eq!(stack.pop().unwrap(), vec![1]);
+        assert_eq!(stack.pop(), None);
+
+        // now that the stack is empty, at attempt
+        // to rotate should result in an error
+        assert!(matches!(execute(stack, parse("EVAL")).wait().err(),
+        Some(Error::EmptyStack(_))));
+    }
+
 }
 
 
@@ -471,6 +659,16 @@ mod textparser {
         s == b'_' || s == b':' || s == b'-'
     }
 
+    fn program_to_vec(p: Vec<Vec<u8>>) -> Vec<u8> {
+        let mut vec = Vec::new();
+        let s = p.iter().fold(0, |s, i| i.len() + s);
+        write_size!(vec, s);
+        for mut item in p {
+            vec.append(&mut item);
+        }
+        vec
+    }
+
     named!(word<Vec<u8>>, do_parse!(
                             word: take_while1!(is_word_char)  >>
                                   (prefix_word(word))));
@@ -482,7 +680,10 @@ mod textparser {
     named!(string<Vec<u8>>, do_parse!(
                              str: delimited!(char!('"'), is_not!("\""), char!('"')) >>
                                   (string_to_vec(str))));
-    named!(item<Vec<u8>>, alt!(binary | string | word));
+    named!(code<Vec<u8>>, do_parse!(
+                             prog: delimited!(char!('['), program, char!(']')) >>
+                                   (program_to_vec(prog))));
+    named!(item<Vec<u8>>, alt!(binary | string | code | word));
     named!(program<Vec<Vec<u8>>>, separated_list!(tag!(" "), item));
 
     /// Parses human-readable PumpkinScript
@@ -542,6 +743,15 @@ mod textparser {
             stack.pop();
             stack.pop();
             assert_eq!(stack.pop(), None);
+        }
+
+        #[test]
+        fn test_code() {
+            let script = parse("[DUP]");
+            let dup = [4, 0x83, b'D', b'U', b'P'];
+            let mut vec: Vec<&[u8]> = Vec::new();
+            vec.push(&dup);
+            assert_eq!(script, vec);
         }
 
     }
