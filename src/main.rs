f571893b25f45cf769f13aa1f75add3dfed622ed
Yurii Rashkovskii <yrashk@gmail.com>
2017-02-10 17:42:38 +0700

    Problem: by default, lmdb map size is small: 10485760
    This effectively limits the database to approx 10Mb in size
    
    Solution: detect available disk space and use that as a map
    size, making the database file virtually unlimited. Until
    we hit more complicated scenarios with multiple files on
    different disks (with merged traversals, etc.)
    
    This is not enabled on Windows as setting this on Windows
    leads to making the file assume the size of the map.

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ extern crate tempdir;
 #[cfg(test)]
 extern crate crossbeam;
 
+extern crate libc;
 
 extern crate futures;
 extern crate tokio_core;
@@ -43,6 +44,10 @@ pub mod server;
 use std::thread;
 
 use std::fs;
+use std::path;
+
+use std::ffi::CString;
+use libc::statvfs;
 
 lazy_static! {
  static ref ENV: lmdb::Environment = {
@@ -51,8 +56,28 @@ lazy_static! {
      let path = config::get_str("storage.path").unwrap().into_owned();
      fs::create_dir_all(path.as_str()).expect("can't create directory");
      unsafe {
-            lmdb::EnvBuilder::new()
-                .expect("can't create env builder")
+            let mut env_builder = lmdb::EnvBuilder::new()
+                .expect("can't create env builder");
+
+            // Configure map size
+            if !cfg!(target_os = "windows") {
+                let path = path::PathBuf::from(path.as_str());
+                let canonical = fs::canonicalize(&path).unwrap();
+                let absolute_path = canonical.as_path().to_str().unwrap();
+                let absolute_path_c = CString::new(absolute_path).unwrap();
+                let mut stat: statvfs = statvfs{f_bsize: 0, f_frsize: 0, f_blocks: 0,
+                                                f_bfree: 0, f_bavail: 0, f_files: 0,
+                                                f_ffree: 0, f_favail: 0, f_fsid: 0,
+                                                f_flag: 0, f_namemax: 0};
+                if statvfs(absolute_path_c.as_ptr(), &mut stat) != 0 {
+                   println!("Can't determine available disk space");
+                } else {
+                   let size = (stat.f_frsize * stat.f_bavail as u64) as usize;
+                   println!("Available disk space is approx. {}Gb, setting database map size to it", size / (1024*1024*1024));
+                   env_builder.set_mapsize(size).expect("can't set map size");
+                }
+            }
+            env_builder
                 .open(path.as_str(), lmdb::open::Flags::empty(), 0o600)
                 .expect("can't open env")
     }
