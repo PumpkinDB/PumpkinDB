f8386ec44a0d5f2681426fb1d5ba09067ea8e06b
Yurii Rashkovskii <yrashk@gmail.com>
2017-04-03 22:45:42 -0700

    Problem: debugging scripts is hard
    There is no easy way to look into what script is doing to be
    able to debug it.
    
    Solution: add experimental TRACE instruction to pumpkindb-term
    (not available outside of it) that allows users to trace any values.
    
    ```
    PumpkinDB> "test" TRACE 1.
    Trace: "test"
    0x01
    ```
    
    Closes #227

--- pumpkindb_term/src/main.rs
+++ pumpkindb_term/src/main.rs
@@ -28,7 +28,7 @@ use rustyline::error::ReadlineError;
 use rustyline::Editor;
 use rustyline::history::History;
 
-use ansi_term::Colour::Red;
+use ansi_term::Colour::{Red, Cyan};
 
 use uuid::Uuid;
 
@@ -37,6 +37,22 @@ use clap::{Arg, App};
 use pumpkindb_engine::script;
 use pumpkinscript::compose::*;
 use pumpkinscript::compose::Item::*;
+
+fn print_item(s: &mut String, data: &[u8]) {
+    if data.iter()
+        .all(|c| *c >= 0x20 && *c <= 0x7e) {
+        let _ = write!(s,
+                       "{:?} ",
+                       str::from_utf8(data)
+                           .unwrap());
+    } else {
+        let _ = write!(s, "0x");
+        for b in Vec::from(data) {
+            let _ = write!(s, "{:02x}", b);
+        }
+        let _ = write!(s, " ");
+    }
+}
 
 fn main() {
 
@@ -104,85 +120,114 @@ fn main() {
                     match pumpkinscript::parse(&program) {
                         Ok(compiled) => {
                             let uuid = Uuid::new_v4();
-                            let msg: Vec<u8> = Program(vec![Data(&compiled),
-                                                            Instruction("TRY"),
+                            let trace: Vec<u8> = Program(vec![
+                                Data(&[1]), Instruction("WRAP"),
+                                Data("TRACE".as_bytes()),
+                                Instruction("SWAP"),
+                                Instruction("CONCAT"),
+                                Data(uuid.as_bytes()),
+                                Instruction("PUBLISH"),
+                               ]).into();
+                            let msg: Vec<u8> = Program(vec![
                                                             Data(uuid.as_bytes()),
-                                                            InstructionRef("topic"),
-                                                            Instruction("SET"),
-                                                            Instruction("STACK"),
-                                                            Instruction("topic"),
                                                             Instruction("SUBSCRIBE"),
+                                                            InstructionRef("___subscription___"),
+                                                            Instruction("SET"),
+                                                            Data(&trace),
+                                                            InstructionRef("TRACE"),
+                                                            Instruction("DEF"),
+                                                            Data(&compiled),
+                                                            Instruction("TRY"),
+                                                            Instruction("STACK"),
+                                                            Data("RESULT".as_bytes()),
                                                             Instruction("SWAP"),
-                                                            Instruction("topic"),
+                                                            Instruction("CONCAT"),
+                                                            Data(uuid.as_bytes()),
                                                             Instruction("PUBLISH"),
-                                                            Instruction("UNSUBSCRIBE")])
-                                .into();
+                                                            Instruction("___subscription___"),
+                                                            Instruction("UNSUBSCRIBE")
+                               ]).into();
                             let mut buf = [0u8; 4];
 
                             BigEndian::write_u32(&mut buf, msg.len() as u32);
                             stream.write_all(buf.as_ref()).unwrap();
                             stream.write_all(msg.as_ref()).unwrap();
 
-                            stream.read(&mut buf).unwrap();
+                            let mut done = false;
 
-                            let msg_len = BigEndian::read_u32(&mut buf);
+                            while !done {
+                                stream.read(&mut buf).unwrap();
+                                let msg_len = BigEndian::read_u32(&mut buf);
 
-                            let s_ref = <TcpStream as Read>::by_ref(&mut stream);
+                                let s_ref = <TcpStream as Read>::by_ref(&mut stream);
 
-                            r.clear();
+                                r.clear();
 
-                            match s_ref.take(msg_len as u64).read_to_end(&mut r) {
-                                Ok(0) => {}
-                                Ok(_) => {
-                                    let mut input = r.clone();
-                                    let mut top_level = true;
-                                    let mut s = String::new();
-                                    while input.len() > 0 {
-                                        match pumpkinscript::binparser::data(input.clone().as_slice()) {
-                                            pumpkinscript::ParseResult::Done(rest, data) => {
-                                                let (_, size) = pumpkinscript::binparser::data_size(data)
-                                                    .unwrap();
-                                                let data = &data[script::offset_by_size(size)..];
+                                match s_ref.take(msg_len as u64).read_to_end(&mut r) {
+                                    Ok(0) => {}
+                                    Ok(_) => {
+                                        if r[0..5].to_vec() == b"TRACE" {
+                                            let input = r[5..msg_len as usize].to_vec();
+                                            let mut s = String::new();
+                                            if cfg!(target_os = "windows") {
+                                                let _ = write!(&mut s, "Trace: ");
+                                            } else {
+                                                let _ = write!(&mut s,
+                                                               "{}", Cyan.paint("Trace: "));
+                                            }
+                                            match pumpkinscript::binparser::data(&input.clone()) {
+                                                pumpkinscript::ParseResult::Done(_, data) => {
+                                                    let (_, size) = pumpkinscript::binparser::data_size(data)
+                                                        .unwrap();
+                                                    let data = &data[script::offset_by_size(size)..];
+                                                    print_item(&mut s, data);
+                                                },
+                                                e => {
+                                                    panic!("{:?}", e);
+                                                }
+                                            }
+                                            println!("{}", s);
+                                        } else if r[0..6].to_vec() == b"RESULT" {
+                                            let mut input = r[6..msg_len as usize].to_vec();
+                                            done = true;
+                                            let mut top_level = true;
+                                            let mut s = String::new();
+                                            while input.len() > 0 {
+                                                match pumpkinscript::binparser::data(&input.clone()) {
+                                                    pumpkinscript::ParseResult::Done(rest, data) => {
+                                                        let (_, size) = pumpkinscript::binparser::data_size(data)
+                                                            .unwrap();
+                                                        let data = &data[script::offset_by_size(size)..];
 
-                                                input = Vec::from(rest);
+                                                        input = Vec::from(rest);
 
-                                                if rest.len() == 0 && top_level {
-                                                    top_level = false;
-                                                    if data.len() > 0 {
-                                                        if cfg!(target_os = "windows") {
-                                                            let _ = write!(&mut s, "Error: ");
+                                                        if rest.len() == 0 && top_level {
+                                                            top_level = false;
+                                                            if data.len() > 0 {
+                                                                if cfg!(target_os = "windows") {
+                                                                    let _ = write!(&mut s, "Error: ");
+                                                                } else {
+                                                                    let _ = write!(&mut s,
+                                                                                   "{}",
+                                                                                   Red.paint("Error: "));
+                                                                }
+                                                                input = Vec::from(data);
+                                                            }
                                                         } else {
-                                                            let _ = write!(&mut s,
-                                                                           "{}",
-                                                                           Red.paint("Error: "));
+                                                            print_item(&mut s, data);
                                                         }
-                                                        input = Vec::from(data);
                                                     }
-                                                } else {
-                                                    if data.iter()
-                                                        .all(|c| *c >= 0x20 && *c <= 0x7e) {
-                                                        let _ = write!(&mut s,
-                                                                       "{:?} ",
-                                                                       str::from_utf8(data)
-                                                                           .unwrap());
-                                                    } else {
-                                                        let _ = write!(&mut s, "0x");
-                                                        for b in Vec::from(data) {
-                                                            let _ = write!(&mut s, "{:02x}", b);
-                                                        }
-                                                        let _ = write!(&mut s, " ");
+                                                    e => {
+                                                        panic!("{:?}", e);
                                                     }
                                                 }
                                             }
-                                            e => {
-                                                panic!("{:?}", e);
-                                            }
+                                            println!("{}", s);
                                         }
                                     }
-                                    println!("{}", s);
-                                }
-                                Err(e) => {
-                                    panic!("{}", e);
+                                    Err(e) => {
+                                        panic!("{}", e);
+                                    }
                                 }
                             }
                         }
