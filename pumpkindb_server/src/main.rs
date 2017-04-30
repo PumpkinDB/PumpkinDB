ec867df34874a6d57f527fc5214752b02b79ffc5
Yurii Rashkovskii <yrashk@gmail.com>
2017-04-30 09:32:39 -0700

    Problem: unused `test` feature in pumpkindb_server
    ```
     --> pumpkindb_server/src/main.rs:7:27
      |
    7 | #![cfg_attr(test, feature(test))]
      |                           ^^^^
      |
      = note: #[warn(unused_features)] on by default
    ```
    
    Solution: drop the feature

--- pumpkindb_server/src/main.rs
+++ pumpkindb_server/src/main.rs
@@ -4,7 +4,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #![feature(slice_patterns, advanced_slice_patterns)]
-#![cfg_attr(test, feature(test))]
 
 extern crate mio;
 extern crate memmap;
